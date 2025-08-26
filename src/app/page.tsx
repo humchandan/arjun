a1e5e232d4080c5b344630bdfb2f06b7be146d12
Chandan Shaw <33323620+humchandan@users.noreply.github.com>
2025-08-26 14:53:50 +0530

    04-trpc with inngest background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
+ "use client";
 
-
-
-
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from '@tanstack/react-query';
+import { toast } from "sonner";
 const Page = () => {
-  
+ const trpc = useTRPC();
+ const invoke = useMutation(trpc.invoke.mutationOptions({
+onSuccess: () => {
+  toast.success("Background job started!");
 
-  return <div>
-    HELLO Arjun
+ }
+}));
+
+  return (
+    <div className="p-4 max-w-7xl mx-auto">
+      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ text: "Arjun" })} >
+       Invoke Background Function 
+      </Button>
     </div>
+  );
+
 }
-export default Page 
+export default Page;           
