179baeb7673422c71702bb521e5040c8965d5a18
Chandan Shaw <33323620+humchandan@users.noreply.github.com>
2025-08-27 18:47:29 +0530

    09-Projects

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,11 @@
 import { messageRouter } from '@/modules/messages/server/procedures';
+import { projectsRouter } from '@/modules/projects/server/procedures';
+
 import { createTRPCRouter } from '../init';
 
 export const appRouter = createTRPCRouter({
 messages: messageRouter,
+projects: projectsRouter
 
 });
 
